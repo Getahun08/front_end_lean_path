c0223e3b81233dad33c7934777f0b134825fb76d
Getahun08! <getahuntamirat08@gmail.com>
2025-03-27 02:03:34 -0700

    added arry and function

--- frontend/index.js
+++ frontend/index.js
@@ -4,3 +4,11 @@ person={
  lastName:"tame",
  password:"2"};
 console.log(person)
+let colorSelector=['red','green']
+colorSelector[2]="blue"
+console.log(colorSelector.length)
+function greet(firstName,lastName){
+console.log("hellow"+firstName+lastName)
+
+}
+greet();
